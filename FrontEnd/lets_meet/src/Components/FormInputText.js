860bae069d8f35bbda7b30dd5f43223df68a32c8
Nickolas A. Morrison <nickmorrison09@gmail.com>
2017-11-29 14:50:53 -0500

    Added form input text

--- FrontEnd/lets_meet/src/Components/FormInputText.js
+++ FrontEnd/lets_meet/src/Components/FormInputText.js
@@ -1,5 +1,51 @@
 import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'proptypes';
+
+import './FormInputText.css';
 
 class FormInputText extends React.Component{
-	
+
+	constructor(props){
+		super(props);
+		this.onChange = this.onChange.bind(this);
+		this.onKeyDown = this.onKeyDown.bind(this);
+	}
+
+	componentWillMount(){
+		this.setState({
+			text: ""
+		});
+	}
+
+	onKeyDown(evt){
+		if(evt.key === "Enter" && this.props.onDone){
+			this.props.onDone(evt.target.value);
+		}
+	}
+
+	onChange(evt){	
+		this.setState({
+			text: evt.target.value
+		});
+	}
+
+	render(){
+		const hintClassName = classNames("hint-text",{
+			"top": this.state.text != null && this.state.text.length > 0
+		});
+		return (
+			<div className="form-input-text">			
+				<div className={hintClassName}>{this.props.prompt}</div>
+				<input type="text" onChange={this.onChange} onKeyDown={this.onKeyDown}/>				
+			</div>
+		)
+	}
 }
+
+FormInputText.propTypes = {
+	prompt: PropTypes.string.isRequired,
+	onDone: PropTypes.func
+}
+
+export default FormInputText;
--- FrontEnd/lets_meet/src/Create/Create.js
+++ FrontEnd/lets_meet/src/Create/Create.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import ProgressIndicator from '../Components/ProgressIndicator.js';
+import FormInputText from '../Components/FormInputText';
 
 class Create extends React.Component{
 	render(){
 		return (
 			<div className="page">
 				<ProgressIndicator count={3}/>
+				<FormInputText prompt="Event Name" onDone={(text)=>{alert("DONE "+text)}}/>
 			</div>
 		)
 	}
